b70f8cf9db2140836c208863e0abf791bbf57b29
Heider Lopes <heidertreinamentos@gmail.com>
2020-10-20 23:05:23 -0300

    Implementacao do MVVM

--- app/src/main/java/br/com/heiderlopes/padroesdeproejtoandroid/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/heiderlopes/padroesdeproejtoandroid/ui/main/MainActivity.kt
@@ -2,11 +2,38 @@ package br.com.heiderlopes.padroesdeproejtoandroid.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import br.com.heiderlopes.padroesdeproejtoandroid.R
+import br.com.heiderlopes.padroesdeproejtoandroid.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        initDataBinding()
+
+        registerObserver()
+
+    }
+
+    private fun initDataBinding() {
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainViewModel = mainViewModel
+        binding.lifecycleOwner = this
+    }
+
+    private fun registerObserver() {
+        mainViewModel.mensagemErro.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        })
     }
 }
---
+++ app/src/main/java/br/com/heiderlopes/padroesdeproejtoandroid/ui/main/MainViewModel.kt
@@ -0,0 +1,38 @@
+package br.com.heiderlopes.padroesdeproejtoandroid.ui.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.com.heiderlopes.padroesdeproejtoandroid.data.network.APIService
+import br.com.heiderlopes.padroesdeproejtoandroid.model.Endereco
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+
+
+    val enderecoResponse = MutableLiveData<Endereco>()
+    val mensagemErro = MutableLiveData<String>()
+
+    fun pesquisar(cep: String) {
+
+        APIService.instance
+            ?.pesquisar(cep)
+            ?.enqueue(object: Callback<Endereco> {
+                override fun onResponse(call: Call<Endereco>, response: Response<Endereco>) {
+                    if(response.isSuccessful) {
+                        enderecoResponse.value = response.body()
+                    } else {
+                        mensagemErro.value = "Endereço não encontrado"
+                    }
+                }
+
+                override fun onFailure(call: Call<Endereco>, t: Throwable) {
+                    mensagemErro.value = t.message.toString()
+                }
+            })
+
+    }
+
+
+}
